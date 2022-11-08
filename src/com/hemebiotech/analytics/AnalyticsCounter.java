380c7c07c8c3211e9512c3f42b0e0a87e11e1607
PSI <paul.siou@inetum.com>
2022-11-08 16:21:46 +0100

    Factorized main method

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,15 +7,12 @@ public class AnalyticsCounter
     public static void main(String[] args) throws Exception
     {
         //Get symptoms list from SymptomReader
-        SymptomReader reader = new SymptomReader("symptoms.txt");
-        ArrayList<String> symptomsList= new ArrayList<>(reader.GetSymptoms());
+        ArrayList<String> symptomsList= new ArrayList<>(new SymptomReader("symptoms.txt").GetSymptoms());
 
         //Count symptoms by type and organize them alphabetically
-        SymptomAnalyzer analyzer = new SymptomAnalyzer(symptomsList);
-        symptomsList = analyzer.analyze();
+        symptomsList = new SymptomAnalyzer(symptomsList).analyze();
 
         //Output Symptoms list
-        OutputWriter writer = new OutputWriter(symptomsList);
-        writer.writeOutput();
+        new OutputWriter(symptomsList).writeOutput();
     }
 }
