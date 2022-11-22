833b68e55c92e4b6bb24d8ed5bacc23ada874786
Psolyvem <psolyvem@gmail.com>
2022-11-22 22:56:44 +0100

    Changed the output.txt in a results.out

--- src/com/hemebiotech/analytics/OutputWriter.java
+++ src/com/hemebiotech/analytics/OutputWriter.java
@@ -3,19 +3,18 @@ package com.hemebiotech.analytics;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class OutputWriter implements IOutputWriter
 {
     /**
-     * Write the output in a .txt file, based on the list given in the constructor.
+     * Write the output in a .out file, based on the list given in the constructor.
      * To change the list, use SetOutput
      */
     @Override
     public void writeOutput(List<String> output) throws IOException
     {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("results.out"));
 
         for (String s : output)
         {
